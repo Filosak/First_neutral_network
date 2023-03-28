7ad725d594a238b1cbd6933731f2eef231e7d412
Filosak <124183617+Filosak@users.noreply.github.com>
2023-03-28 17:42:00 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
+import matplotlib.pyplot as plt
 import numpy as np
-
+import pandas
 
 class Dense_layer():
     def __init__(self, input_size, output_size):
@@ -11,10 +12,14 @@ class Dense_layer():
         self.input = input
         return np.dot(input, self.weights) + self.biases
     
-    def backward(self, gradient, output):
-        self.weights += output.T.dot(gradient)
-        self.biases += gradient
+    def backward(self, gradient, learning_rate):
+        gradient_delta = gradient * self.d_sigmoid(self.sigmoid_func)
+        gradient_delta = gradient_delta * learning_rate
 
+        self.weights += self.input.T.dot(gradient_delta)
+        self.biases += gradient_delta
+
+        return gradient_delta.dot(self.weights.T)
 
     def sigmoid(self, x):
         self.sigmoid_func = 1.0 / (1.0 + np.exp(-x))
@@ -22,92 +27,63 @@ class Dense_layer():
 
     def d_sigmoid(self, x):
         self.d_sigmoid_func = np.multiply(x, 1.0-x)
-        return self.d_sigmoid_func 
+        return self.d_sigmoid_func
 
 
-class Calculate_error:
-    def calculate_output_error(self, real, expected):
-        return np.subtract(expected, real)
+class Network:
+    def __init__(self):
+        self.learning_rate = 0.5
+        self.setup = [
+            Dense_layer(784, 64),
+            Dense_layer(64, 10)
+        ]
+        self.data = [
+                    [np.array(([[1, 0]]), dtype=float),
+                     np.array(([[1]]), dtype=float)
+                    ],
+                    [np.array(([[1, 1]]), dtype=float),
+                     np.array(([[0]]), dtype=float)
+                    ],
+                    [np.array(([[0, 1]]), dtype=float),
+                     np.array(([[1]]), dtype=float)
+                    ],
+                    [np.array(([[0, 0]]), dtype=float),
+                     np.array(([[0]]), dtype=float)
+                    ]
+                ]
 
 
-input_to_hidden = Dense_layer(2, 4)
-hidden_to_output = Dense_layer(4, 1)
-Error_calc = Calculate_error()
+    def train(self, X, Y):
+        output = X
+        for layer in self.setup:
+            output = layer.sigmoid(layer.forward(output))
 
-data = [
-    [
-    np.array(([[1, 0]]), dtype=float),
-    np.array(([[1]]), dtype=float)
-    ],
+        gradient = Y - output
+        for layer in reversed(self.setup):
+            gradient = layer.backward(gradient, self.learning_rate)
 
-    [
-    np.array(([[1, 1]]), dtype=float),
-    np.array(([[0]]), dtype=float)
-    ],
-
-    [
-    np.array(([[0, 1]]), dtype=float),
-    np.array(([[1]]), dtype=float)
-    ],
-
-    [
-    np.array(([[0, 0]]), dtype=float),
-    np.array(([[0]]), dtype=float)
-    ]
-]
-
-learning_rate = 0.5
+        return output
 
 
-def feedForward(X):
-    hidden_sigmoid = input_to_hidden.sigmoid(input_to_hidden.forward(X))
-    output_sigmoid = input_to_hidden.sigmoid(hidden_to_output.forward(hidden_sigmoid))
-    return output_sigmoid, hidden_sigmoid
-
-def backprop(output_sigmoid, hidden_sigmoid):
-    output_error = Y - output_sigmoid
-    gradient = output_error * hidden_to_output.d_sigmoid(output_sigmoid)
-    gradient = gradient * learning_rate
-
-    hidden_error = gradient.dot(hidden_to_output.weights.T)
-    hidden_gradient = hidden_error * hidden_to_output.d_sigmoid(hidden_sigmoid)
-    hidden_gradient = hidden_gradient * learning_rate
-
-    input_to_hidden.backward(hidden_gradient, X)
-    hidden_to_output.backward(gradient, hidden_sigmoid)
-
-def calculate_loss():
-    loss = 0
-
-    X, Y = data[0]
-    output_sigmoid, hidden_sigmoid = feedForward(X)
-    loss += Y - output_sigmoid
-
-    X, Y = data[1]
-    output_sigmoid, hidden_sigmoid = feedForward(X)
-    loss += Y - output_sigmoid
-
-    X, Y = data[2]
-    output_sigmoid, hidden_sigmoid = feedForward(X)
-    loss += Y - output_sigmoid
-
-    X, Y = data[3]
-    output_sigmoid, hidden_sigmoid = feedForward(X)
-    loss += Y - output_sigmoid
-
-    print(loss)
+    def predict(self, index):
+        return self.train(self.data[index][0] , self.data[index][1])
 
 
-for i in range(20000):
-    X, Y = random.choice(data)
-    output_sigmoid, hidden_sigmoid = feedForward(X)
-    backprop(output_sigmoid, hidden_sigmoid)
+    def train_loop(self, num):
+        for i in range(0, num):
+            X, Y = random.choice(self.data)
+            self.train(X, Y)
 
-    if i % 10 == 0:
-        calculate_loss()
-    
-    
+    def one_hot_encoding(self, Y):
+        pass
 
 
 
 
+nn = Network()
+nn.train_loop(10000)
+
+print(nn.predict(0), "   1")
+print(nn.predict(1), "   0")
+print(nn.predict(2), "   1")
+print(nn.predict(3), "   0")
