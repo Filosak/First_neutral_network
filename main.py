f1808a562af934201045a09518972ca073025ce1
Filosak <124183617+Filosak@users.noreply.github.com>
2023-04-03 17:29:06 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ class Dense_layer():
     def __init__(self, input_size, output_size, acctivation="sigmoid"):
         self.weights = np.random.randn(input_size, output_size)
         self.biases = np.zeros((1, output_size))
-
         self.activation = acctivation
 
     def forward(self, input):
@@ -19,18 +18,26 @@ class Dense_layer():
             return self.sigmoid(self.output)
         elif self.activation == "relu":
             return self.relu(self.output)
+        elif self.activation == "softmax":
+            return self.softmax(self.output)
     
     def backward(self, gradient, learning_rate):
         if self.activation == "sigmoid":
             gradient_delta = gradient * self.d_sigmoid(self.sigmoid_func)
+
         elif self.activation == "relu":
-            gradient_delta = gradient * self.d_relu(self.relu_output)
-        gradient_delta = gradient_delta * learning_rate
+            gradient_delta = 1 / m * gradient.dot(self.output.T)
 
-        self.weights += self.input.T.dot(gradient_delta)
-        self.biases += gradient_delta
+        elif self.activation == "softmax":
+            gradient_delta = gradient * self.d_softmax(self.softmax_func)
 
+        self.weights += learning_rate * self.input.T.dot(gradient_delta)
+        self.biases += learning_rate * gradient_delta
+
+        if self.activation == "relu":
+            return gradient.dot(self.weights.T)
         return gradient_delta.dot(self.weights.T)
+
 
     def sigmoid(self, x):
         self.sigmoid_func = 1.0 / (1.0 + np.exp(-x))
@@ -41,21 +48,28 @@ class Dense_layer():
         return self.d_sigmoid_func
     
     def relu(self, x):
-        self.relu_output = np.maximum(0, x) 
-        return self.relu_output
+        self.relu_func = np.maximum(x, 0)
+        return self.relu_func
 
     def d_relu(self, x):
-        return 1 * (x > 0)
+        return x > 0
+    
+    def softmax(self, x):
+        self.exps = np.exp(x - np.max(x, axis=1, keepdims=True))
+        self.softmax_func = self.exps / np.sum(self.exps, axis=1, keepdims=True)
+        return self.softmax_func
+    
+    def d_softmax(self, x):
+        return x * (1 - x)
 
 
 class Network:
     def __init__(self):
-        self.learning_rate = 0.5
+        self.learning_rate = 0.05
         self.setup = [
             Dense_layer(784, 16),
-            Dense_layer(16, 10),
+            Dense_layer(16, 10, "softmax")
         ]
-
 
     def train(self, X, Y):
         output = X
@@ -69,7 +83,6 @@ class Network:
 
         return output
 
-
     def predict(self, X, Y):
         output = X
         for layer in self.setup:
@@ -77,8 +90,6 @@ class Network:
 
         return np.argmax(output)
         
-
-
     def train_loop(self, num):
         for i in range(0, num):
             X, Y = random.choice(self.data)
@@ -89,10 +100,19 @@ class Network:
         one_hot[Y] = 1
         return one_hot
 
+def checkAccuaracy():
+    wrong = 0
+    for Y, X in dataSetTest:
+        final = nn.predict(X, Y)
+
+        if Y != final:
+            wrong += 1
+    
+    print(wrong)
+    print(100 - wrong / len(dataSetTest) * 100)
+
 
 nn = Network()
-
-
 data = pd.read_csv('project/train.csv')
 data = np.array(data)
 np.random.shuffle(data)
@@ -100,8 +120,6 @@ np.random.shuffle(data)
 m, n = data.shape
 data_train = data[:40000]
 data_test = data[40000:]
-
-print(data_test.shape)
 
 Y_train = data_train[:, 0]
 X_train = data_train[:, 1:] # x je 1D np array skusit to dát do 2D array pls work
@@ -120,30 +138,16 @@ for i,label in enumerate(Y_train):
 for i,label in enumerate(Y_test):
     dataSetTest.append([label, np.array([list(X_test[i])])])
 
-epochs = 25
-for _ in range(epochs):
+epochs = 10
+for i in range(epochs):
     np.random.shuffle(dataSetTrain)
 
     for Y, X in dataSetTrain:
         nn.train(X, Y)
 
-    nn.learning_rate -= nn.learning_rate * 0.2
-
-
-
-wrong = 0
-for Y, X in dataSetTest:
-    final = nn.predict(X, Y)
-
-    if Y != final:
-        wrong += 1
-
-    # image = X.reshape((28,28)) * 255
-    # plt.gray()
-    # plt.imshow(image, interpolation="nearest")
-    # plt.show()
-
-print(wrong)
+    print(i, " Epoch")
+    checkAccuaracy()
+    # nn.learning_rate -= nn.learning_rate * 0.1
 
 
 
@@ -154,3 +158,5 @@ print(wrong)
 # best - 89,55% acc (16 hidden but 25 epochs / 0.1 lr)
 # best - 91,3% acc (16 hidden / 0.2 lr)
 # best - 92% acc (16 hidden but 25 epochs / 0.2 lr)
+# best - 92.7% acc (32 hidden / 16 hidden / 0.5 lr - 10%)
+# best - 93.5% acc (16 hidden 0.5 lr - 10%)
